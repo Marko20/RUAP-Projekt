eac7cae8735b3c3f21e7b1103c5317e5f9581540
Marko20 <marko20hampy@gmail.com>
2018-03-27 13:19:55 +0200

    Dodana nova linija koda printf

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,11 +1,12 @@
 // ConsoleApplication1.cpp : Defines the entry point for the console application.
 //
-
+#include "stdio.h"
 #include "stdafx.h"
 
 
 int main()
 {
+	printf ("Pozdrav iz aplikacije od Marko Hampovèan");
     return 0;
 }
 
